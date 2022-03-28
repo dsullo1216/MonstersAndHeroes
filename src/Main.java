d286ed23ec8a0b22c3b6f0c6997feaea1c35fb20
David Sullo <dsullo@bu.edu>
2022-03-27 22:29:05 -0400

    Finished map and tested in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     
-    public static void main(String[] args) {
-        Inventory test = new Inventory(5);
+    public static void main(String[] args) throws IOException {
+        /*Inventory test = new Inventory(5);
         test.addItem(new Weapon("name", 10, 10, 10));
         Item testI = test.getItemAt(0);
         Weapon testII = new Weapon("name", 10, 10, 10);
@@ -10,6 +13,10 @@ public class Main {
         mapt.printMap();
         System.out.println();
         System.out.print(mapt.moveSquad(new int[] {0,1}));
-        mapt.printMap();
-    }
+        mapt.printMap();*/
+        String[] test = TSVReader.readTSVFile("./src/data/FireSpells.tsv");
+        for (String uh: test) {
+            System.out.println(uh);
+        }
+        }
 }
