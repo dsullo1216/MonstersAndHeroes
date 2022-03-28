d0c31270cac1f67bacd58522588c23fe7bb6b66b
David Sullo <dsullo@bu.edu>
2022-03-27 22:28:40 -0400

    Removed battle rate as it belongs to game not tile

--- src/BattleCell.java
+++ src/BattleCell.java
@@ -1,14 +1,8 @@
 public class BattleCell extends AccessibleCell {
-    
-    private static final double BATTLERATE = 0.125;
 
     public BattleCell(int[] position) {
         super('_', position);
         type = "Battle";
-    }
-
-    public double getBattleRate() {
-        return BATTLERATE;
     }
 
 }
