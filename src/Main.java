729bc58565eeb7138d24c3ccb4d9fe232c2d3d4a
David Sullo <dsullo@bu.edu>
2022-03-28 14:43:17 -0400

    Changed for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     
@@ -13,12 +14,15 @@ public class Main {
         mapt.printMap();
         System.out.println();
         System.out.print(mapt.moveSquad(new int[] {0,1}));
-        mapt.printMap();*/
+        mapt.printMap();
         Hero[] test4 = ReadFiles.ListOfHeroes();
         System.out.println(test4[0].description());
         for (int i = 0; i < test4.length; i++) {
             System.out.print(i + ". ");
             System.out.println(test4[i]);
-        }
+        }*/
+        Game test = new HeroesAndMonstersGame();
+        Scanner sc = new Scanner(System.in);
+        test.playGame(sc);
         }
 }
