4e817ebfe414e42bb975f1c533355b2a768fc646
David Sullo <dsullo@bu.edu>
2022-03-28 02:27:01 -0400

    Added IOException to handle file input

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,11 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public abstract class Game {
  
     protected Map map;
 
-    public abstract void launchGame(Scanner sc);
+    public abstract void launchGame(Scanner sc) throws IOException;
 
     public abstract void processInput(Scanner sc);
 
