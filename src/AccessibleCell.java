6230f42e1ad15b0d6fc3438b681b1abf5f447fe9
David Sullo <dsullo@bu.edu>
2022-03-27 21:24:23 -0400

    Added setSquad and removeSquad methods to change boolean

--- src/AccessibleCell.java
+++ src/AccessibleCell.java
@@ -15,6 +15,16 @@ public abstract class AccessibleCell extends Cell {
         return symbol;
     }
 
+    public boolean setSquadHere() {
+        this.isHeroSquadHere = true;
+        return this.isHeroSquadHere;
+    }
+
+    public boolean removeSquadHere() {
+        this.isHeroSquadHere = false;
+        return this.isHeroSquadHere;
+    }
+
     /* TODO ADD Trigger_Event Interface and each respective event to each tile */
  
 }
