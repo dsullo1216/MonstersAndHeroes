07b9d55911753127588683d5bde2aba324e23aa2
David Sullo <dsullo@bu.edu>
2022-03-28 23:19:55 -0400

    Added readme

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,3 +1,4 @@
+// Specialized version of Monster that includes title
 public class Dragon extends Monster {
     
     public Dragon(String name, int level, int damage, int defense, int dodgeChance) {
