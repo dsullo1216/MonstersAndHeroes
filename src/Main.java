d34d0583be7dec2a4f17b268bd5d5b8b663cde07
David Sullo <dsullo@bu.edu>
2022-03-28 20:08:58 -0400

    cleaned up Main

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,9 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) throws IOException {
-        /*Inventory test = new Inventory(5);
-        test.addItem(new Weapon("name", 10, 10, 10));
-        Item testI = test.getItemAt(0);
-        Weapon testII = new Weapon("name", 10, 10, 10);
-        System.out.println(testII.equals(testI));
-        Map mapt = new Map();
-        mapt.printMap();
-        System.out.println();
-        System.out.print(mapt.moveSquad(new int[] {0,1}));
-        mapt.printMap();
-        Hero[] test4 = ReadFiles.ListOfHeroes();
-        System.out.println(Hero.DESCRIPTION);
-        for (int i = 0; i < test4.length; i++) {
-            System.out.print(i + ". ");
-            System.out.println(test4[i]);
-        }*/
         Game test1 = new HeroesAndMonstersGame();
         Scanner sc = new Scanner(System.in);
         test1.playGame(sc);
-        MarketCell test = new MarketCell(new int[] {0,0});
-        Inventory testInv = test.getStock();
-        for (int i = 0; i < testInv.size(); i++) {
-            System.out.println(testInv.getItemAt(i));
-        }
-        }
+        // TODO FIGURE OUT WHY READING TSV FILES ISNT WORKING
+    }
 }
