f528453ff5df8a714719f863c64d2e07794d038e
David Sullo <dsullo@bu.edu>
2022-03-28 20:39:11 -0400

    Added randomizer to generating hoarde

--- src/MonsterSquad.java
+++ src/MonsterSquad.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+import java.util.*;
+
 public class MonsterSquad extends Squad{
     
     public MonsterSquad(int size) {
         this.party = new Monster[size];
     }
     
+    public void randomizeMonsterSquad() throws IOException {
+        Monster[] monsterList = ReadFiles.ListOfMonsters();
+        Collections.shuffle(Arrays.asList(monsterList));
+        for (int i = 0; i < party.length; i++) {
+            addEntity(monsterList[i]);
+        }
+    }
 }
